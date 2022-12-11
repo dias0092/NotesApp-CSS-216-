a3c53898b358c306b7eb0febfb81a4e8f051ff5a
Dias Mukhametrakhim <190103159@stu.sdu.edu.kz>
2022-12-11 14:04:04 +0600

    version 0.1

--- app/src/main/java/kz/sdu190103159/notesappcss_216/MainActivity.kt
+++ app/src/main/java/kz/sdu190103159/notesappcss_216/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kz.sdu190103159.notesappcss_216.ui.theme.NotesAppCSS216Theme
 import kz.sdu190103159.notesappcss_216.navigation.NoteNavHst
 
@@ -18,17 +19,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppCSS216Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    NoteNavHst()
-                }
+              Scaffold(topBar = {
+                  TopAppBar( title = { Text(text = "Notes")} , backgroundColor = Color.Blue , contentColor = Color.White , elevation =  12.dp )
+
+
+              }  ,
+                  content = {
+                      Surface(
+                          modifier = Modifier.fillMaxSize(),
+                          color = MaterialTheme.colors.background
+                      ) {
+                          NoteNavHst()
+                      }
+                  })
+
+              }
             }
         }
     }
-}
+
 
 
 
