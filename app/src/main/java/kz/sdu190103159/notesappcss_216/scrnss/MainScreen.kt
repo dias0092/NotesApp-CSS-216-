62b0d0639930e8e616502f017c3170fcacd26280
190103160@stu.sdu.edu.kz <190103160@stu.sdu.edu.kz>
2022-12-11 16:04:56 +0600

    Test

--- app/src/main/java/kz/sdu190103159/notesappcss_216/scrnss/MainScreen.kt
+++ app/src/main/java/kz/sdu190103159/notesappcss_216/scrnss/MainScreen.kt
@@ -34,7 +34,7 @@ fun MainScreen(navController: NavHostController) {
         }
     ){
         Column(){
-            NoteItem(title = "Zhuzha" , subtitle = "Zhuzha molodec" , navController = navController)
+            NoteItem(title = "Oreo chert" , subtitle = "Zhuzha molodec" , navController = navController)
             NoteItem(title = "Zhuzha 0" , subtitle = "Zhuzha molodec 0" , navController = navController)
             NoteItem(title = "Zhuzha -1" , subtitle = "Zhuzha molodec -1 " , navController = navController)
             NoteItem(title = "Zhuzha 1" , subtitle = "Zhuzha molodec 1" , navController = navController)
